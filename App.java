7f06a90c53928133e766fe7b802413f7bd975f83
BrunoriAlberto <69870852+Alby05@users.noreply.github.com>
2024-03-27 10:29:15 +0100

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         ChatServer server = new ChatServer();
-        ChatClient client1 = new ChatClient("Francesco");
-        ChatClient client2 = new ChatClient("Luca");
-        ChatClient client3 = new ChatClient("Mattia");
-        ChatClient client4 = new ChatClient("Anna");
+        ChatClient client1 = new ChatClient("Alberto");
+        ChatClient client2 = new ChatClient("Giulio");
     }
 }
